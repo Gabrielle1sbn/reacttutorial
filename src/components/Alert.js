5b8cb2313abac4f1264beddc7a756d7c1178025b
gabrielle sbn <gabriellesbn15@gmail.com>
2023-03-09 17:44:36 -0300

    create alert component

---
+++ src/components/Alert.js
@@ -0,0 +1,23 @@
+const typeColor = {
+    success: "#cdefd6",
+    error: '#efcdcd',
+    info: '#cddeef',
+    warning: '#ebe5b7'
+}
+
+const Alert = ({type, children}) => {
+
+    const bg = {
+        background: typeColor[type],
+        padding: '20px',
+        borderRadius: '10px'
+    }    
+
+    return (
+        <div style={bg}>
+            {children}
+        </div>
+    )
+}
+
+export default Alert
