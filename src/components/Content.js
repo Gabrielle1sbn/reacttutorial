de04b379bc48d6beee0cc4b393b2e2ce84f19348
gabrielle sbn <gabriellesbn15@gmail.com>
2023-03-16 17:44:58 -0300

    sample of usestate

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,12 +1,12 @@
 import Box from '@mui/material/Box'
 import Alert from './Alert'
+import SampleUseState from './SampleUseState'
 
 const Content = () => {
     return (
         <Box sx={{
             background: "#EEE",
             width: 500,
-            height: 500,
             padding: '15px'
         }}>
             <h1>Content</h1>
@@ -14,19 +14,20 @@ const Content = () => {
                 <Alert type="success" >
                     Deu certo!
                 </Alert>
-
+                <br />
                 <Alert type="error">
                     Deu Erro!
                 </Alert>
-
+                <br />
                 <Alert type="info">
                     Info!
                 </Alert>
-
+                <br />
                 <Alert type="warning">
-                   Warning!
-                </Alert>   
-            
+                   Warning! {/* children */}
+                </Alert>
+
+                <SampleUseState />
         </Box>
     )
 }
