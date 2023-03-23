b8fc020b976f567aaf49ddd0d090c1c4d3faf745
gabrielle sbn <gabriellesbn15@gmail.com>
2023-03-23 17:45:28 -0300

    create app router

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import {Route, Routes} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Error404 from './pages/Error404'
+import Contact from './pages/Contact'
 
 const Router = () => {
     return( 
@@ -9,6 +10,7 @@ const Router = () => {
             <Route path="/" element={<Home />} />
             <Route path="/quem-somos" element={<About />} />
             <Route path="*" element={<Error404 />} />
+            <Route path="/contato" element={<Contact />} />
         </Routes>
     )
 }
