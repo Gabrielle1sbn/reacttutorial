4135ef700c7fa1b2be7589e72c2b173bed891784
Gabrielle1sbn <gabriellesbn15@gmail.com>
2023-04-14 16:49:25 -0300

    add user route

--- src/Router.js
+++ src/Router.js
@@ -3,14 +3,16 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Error404 from './pages/Error404'
 import Contact from './pages/Contact'
+import Users from './pages/Users'
 
 const Router = () => {
     return( 
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/quem-somos" element={<About />} />
-            <Route path="*" element={<Error404 />} />
+            <Route path="/users" element={<Users />} />
             <Route path="/contato" element={<Contact />} />
+            <Route path="*" element={<Error404 />} />
         </Routes>
     )
 }
