6911e90058ca9d9a0e9273421d0ca624d9d518fc
Gabrielle1sbn <gabriellesbn15@gmail.com>
2023-04-14 17:07:52 -0300

    change component content

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,33 +1,16 @@
 import Box from '@mui/material/Box'
-import Alert from './Alert'
-import SampleUserState from './SampleUserState'
 
-const Content = () => {
+
+const Content = ({title, children}) => {
     return (
         <Box sx={{
             background: "#EEE",
             width: 500,
             padding: '15px'
         }}>
-            <h1>Content</h1>
-            
-                <Alert type="success" >
-                    Deu certo!
-                </Alert>
-                <br />
-                <Alert type="error">
-                    Deu Erro!
-                </Alert>
-                <br />
-                <Alert type="info">
-                    Info!
-                </Alert>
-                <br />
-                <Alert type="warning">
-                   Warning! {/* children */}
-                </Alert>
+            <h1>{title}</h1>
 
-                <SampleUserState />
+            {children}
         </Box>
     )
 }
