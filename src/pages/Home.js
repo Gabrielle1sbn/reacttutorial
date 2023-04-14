e5622f38051b6af6276238da2911cf433b141aff
Gabrielle1sbn <gabriellesbn15@gmail.com>
2023-04-14 16:56:53 -0300

    remove main menu add footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,40 @@
 import Box from '@mui/material/Box'
 import Header from '../components/Header'
-import MainMenu from '../components/MainMenu'
 import Sidebar from '../components/Sidebar'
 import Content from '../components/Content'
 import Footer from '../components/Footer'
+import Alert from '../components/Alert'
+import SampleUserState from '../components/SampleUserState'
 
 const Home = () => {
   return (
     <>
       <Header />
-      <MainMenu />
       <Box sx={{
         display: 'flex',
       }}>
-        <Sidebar />
-        <Content />
-      </Box>
+          <Sidebar />
+          
+          <Content title="Home">
+              <Alert type="success" >
+                Deu certo!
+              </Alert>
+              <br />
+              <Alert type="error">
+                Deu Erro!
+              </Alert>
+              <br />
+              <Alert type="info">
+                Info!
+              </Alert>
+              <br />
+              <Alert type="warning">
+                Warning!
+              </Alert> 
+            </Content >
+
+          <SampleUserState />
+        </Box>
       <Footer />
     </>
   )
